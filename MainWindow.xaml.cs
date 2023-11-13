4044c1a79d73c577c70f810d3f4a909952a3efb4
Harla_oq45@student.itstep.org <Harla_oq45@student.itstep.org>
2023-11-13 15:51:31 +0100

    task number 4 done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Windows;
 
-namespace SimpleWpfResume
+namespace BirthdayCalendarApp
 {
     public partial class MainWindow : Window
     {
@@ -10,33 +10,16 @@ namespace SimpleWpfResume
             InitializeComponent();
         }
 
-        private void ShowResumeButton_Click(object sender, RoutedEventArgs e)
+        private void ShowCalendarButton_Click(object sender, RoutedEventArgs e)
         {
-            ShowResume();
-        }
-
-        private void ShowResume()
-        {
-            string resume = "My name is Viktor. I am a student. ";
-            resume += "I have experience with programming languages such as C# and C++. ";
-            resume += "I can develop applications using WPF technology.";
-
-            MessageBox.Show(resume, "Resume - Part 1", MessageBoxButton.OK);
-
-            resume += " I also have skills in working with databases, particularly SQL Server and MySQL. ";
-            resume += "I understand the basics of algorithms and data structures. ";
-            resume += "I am ready to learn new technologies and continuously grow professionally.";
-
-            MessageBox.Show(resume, "Resume - Part 2", MessageBoxButton.OK);
-
-            resume += " I have teamwork skills, as well as experience in problem-solving and finding effective solutions. ";
-            resume += "I am ready to work in a dynamic environment and tackle challenging tasks.";
-
-            MessageBox.Show(resume, "Resume - Part 3", MessageBoxButton.OK);
-
-            int averageCharacters = resume.Length / 3;
-
-            MessageBox.Show($"Average number of characters per page: {averageCharacters}", "Statistics", MessageBoxButton.OK);
+            if (DateTime.TryParse(BirthdayTextBox.Text, out DateTime birthday))
+            {
+                BirthdayCalendar.SelectedDate = birthday;
+            }
+            else
+            {
+                MessageBox.Show("Некорректный формат даты. Используйте формат дд.мм.гггг.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
