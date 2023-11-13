9bcb69cb996dbd0df8aae83804099cc69033410f
Harla_oq45@student.itstep.org <Harla_oq45@student.itstep.org>
2023-11-13 22:13:58 +0100

    second task done

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,42 +1,136 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
+using System.Xml.Serialization;
 
-namespace SimpleWpfResume
+namespace UserInformationApp
 {
     public partial class MainWindow : Window
     {
+        private List<User> users = new List<User>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void ShowResumeButton_Click(object sender, RoutedEventArgs e)
+        private void AddUserButton_Click(object sender, RoutedEventArgs e)
         {
-            ShowResume();
+            User newUser = new User
+            {
+                FirstName = tbFirstName.Text,
+                LastName = tbLastName.Text,
+                Email = tbEmail.Text,
+                Phone = tbPhone.Text
+            };
+
+            users.Add(newUser);
+            UpdateUserListBox();
+            ClearInputFields();
         }
 
-        private void ShowResume()
+        private void EditUserButton_Click(object sender, RoutedEventArgs e)
         {
-            string resume = "My name is Viktor. I am a student. ";
-            resume += "I have experience with programming languages such as C# and C++. ";
-            resume += "I can develop applications using WPF technology.";
+            if (userListBox.SelectedIndex != -1)
+            {
+                User selectedUser = users[userListBox.SelectedIndex];
+                tbFirstName.Text = selectedUser.FirstName;
+                tbLastName.Text = selectedUser.LastName;
+                tbEmail.Text = selectedUser.Email;
+                tbPhone.Text = selectedUser.Phone;
+            }
+        }
 
-            MessageBox.Show(resume, "Resume - Part 1", MessageBoxButton.OK);
+        private void DeleteUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (userListBox.SelectedIndex != -1)
+            {
+                users.RemoveAt(userListBox.SelectedIndex);
+                UpdateUserListBox();
+                ClearInputFields();
+            }
+        }
 
-            resume += " I also have skills in working with databases, particularly SQL Server and MySQL. ";
-            resume += "I understand the basics of algorithms and data structures. ";
-            resume += "I am ready to learn new technologies and continuously grow professionally.";
+        private void ExportTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllLines("users.txt", users.ConvertAll(user => user.ToString()));
+            MessageBox.Show("Информация о пользователях экспортирована в текстовый файл.", "Экспорт", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
 
-            MessageBox.Show(resume, "Resume - Part 2", MessageBoxButton.OK);
+        private void ImportTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines("users.txt");
+                users.Clear();
 
-            resume += " I have teamwork skills, as well as experience in problem-solving and finding effective solutions. ";
-            resume += "I am ready to work in a dynamic environment and tackle challenging tasks.";
+                foreach (string line in lines)
+                {
+                    string[] userInfo = line.Split('|');
+                    if (userInfo.Length == 4)
+                    {
+                        User importedUser = new User
+                        {
+                            FirstName = userInfo[0].Trim(),
+                            LastName = userInfo[1].Trim(),
+                            Email = userInfo[2].Trim(),
+                            Phone = userInfo[3].Trim()
+                        };
 
-            MessageBox.Show(resume, "Resume - Part 3", MessageBoxButton.OK);
+                        users.Add(importedUser);
+                    }
+                }
 
-            int averageCharacters = resume.Length / 3;
+                UpdateUserListBox();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при импорте: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
 
-            MessageBox.Show($"Average number of characters per page: {averageCharacters}", "Statistics", MessageBoxButton.OK);
+        private void ExportXmlButton_Click(object sender, RoutedEventArgs e)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<User>));
+            using (TextWriter writer = new StreamWriter("users.xml"))
+            {
+                xmlSerializer.Serialize(writer, users);
+            }
+
+            MessageBox.Show("Информация о пользователях экспортирована в XML файл.", "Экспорт", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void ImportXmlButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<User>));
+                using (TextReader reader = new StreamReader("users.xml"))
+                {
+                    users = (List<User>)xmlSerializer.Deserialize(reader);
+                }
+
+                UpdateUserListBox();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при импорте: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void UpdateUserListBox()
+        {
+            userListBox.Items.Clear();
+            userListBox.Items.AddRange(users.ToArray());
+        }
+
+        private void ClearInputFields()
+        {
+            tbFirstName.Clear();
+            tbLastName.Clear();
+            tbEmail.Clear();
+            tbPhone.Clear();
         }
     }
 }
